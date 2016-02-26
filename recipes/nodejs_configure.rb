8210c961dc4ffaad1ff25a5faba553e8b7e38edc
Steven Kroh <sk.kroh@gmail.com>
2016-02-26 16:30:11 -0500

    Set SERVER_DOMAIN to own IP or ELB

--- recipes/nodejs_configure.rb
+++ recipes/nodejs_configure.rb
@@ -24,39 +24,70 @@ stack_name = stack['name']
 log "stack name: #{stack_name}"
 
 database_layer = search('aws_opsworks_layer', 'shortname:database').first
-id = database_layer['layer_id']
+id_d = database_layer['layer_id']
 
-log "database layer id: #{id}"
+log "database layer id: #{id_d}"
+
+application_layer = search('aws_opsworks_layer', 'shortname:application').first
+id_a = application_layer['layer_id']
+
+log "application layer id: #{id_a}"
 
 # Start by assuming there is not databsase server instance to connect to
+couch_domain = false
 domain = false
 
 # The development stack only supports a single database server instance.
 # In this case, connect to it via its private ip. The database server will
-# permit connections by security group rule.
+# permit connections by security group rule. Connect to the app server via its
+# public IP.
 if stack_name.include? 'dev'
-  instance = search('aws_opsworks_instance', "layer_ids:#{id}").first
-  domain = instance['private_ip'] if instance
+  instance = search('aws_opsworks_instance', "layer_ids:#{id_d}").first
+  couch_domain = instance['private_ip'] if instance
+  domain = instance['public_ip'] if instance
 # The production and staging stacks manage multiple database servers behind
-# an elastic load balancer. Connect by the ELB's dns name.
+# an elastic load balancer. Connect by the ELB's dns name. Connect to the
+# app server via its ELB's dns name.
 else
-  elb = search('aws_opsworks_elastic_load_balancer', "layer_id:#{id}").first
+  elb = search('aws_opsworks_elastic_load_balancer', "layer_id:#{id_d}").first
+  couch_domain = elb['dns_name'] if elb
+
+  elb = search('aws_opwsorks_elastic_load_balancer', "layer_id:#{id_a}").first
   domain = elb['dns_name'] if elb
 end
 
 # If there is a database server, set SERVER_COUCH_DOMAIN. Otherwise, unset
-# the variable. Unsetting the variable will cause the api service
-if domain
-  log "database server domain: #{domain}"
+# the variable. Unsetting the variable will cause the api service to use the
+# default from the config YAMLs.
+if couch_domain
+  log "database server couch_domain: #{couch_domain}"
 
   env 'SERVER_COUCH_DOMAIN' do
+    value couch_domain
+    action :create
+  end
+else
+  log 'database server couch_domain: NA'
+
+  env 'SERVER_COUCH_DOMAIN' do
+    action :delete
+  end
+end
+
+# If we can find our own address, set SERVER_DOMAIN. Otherwise, unset
+# the variable. Unsetting the variable will cause the api service to use
+# the default form the config YAMLs.
+if couch_domain
+  log "application server domain: #{domain}"
+
+  env 'SERVER_DOMAIN' do
     value domain
     action :create
   end
 else
-  log 'database server domain: NA'
+  log 'application server domain: NA'
 
-  env 'SERVER_COUCH_DOMAIN' do
+  env 'SERVER_DOMAIN' do
     action :delete
   end
 end
